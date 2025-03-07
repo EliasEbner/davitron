2eb7f1c2ba56962fede1ba46b91fd0426ab688d2
Elias Ebner <eliase229@gmail.com>
2025-03-08 00:30:32 +0100

    planet

--- src/planet.rs
+++ src/planet.rs
@@ -21,19 +21,7 @@ impl Entity for Planet {
         // TODO: I don't know what to do here
     }
 
-    // only draw a planet if it is shown in the screen
     fn draw(self: &mut Self, camera: &Camera2D) {
-        if self.position.x < camera.target.x + camera.offset.x + screen_width()
-            && self.position.x > camera.target.x + camera.offset.x - screen_width()
-            && self.position.y < camera.target.y + camera.offset.y + screen_height()
-            && self.position.y > camera.target.y + camera.offset.y - screen_height()
-        {
-            draw_circle(
-                self.position.x - camera.target.x + camera.offset.x,
-                self.position.y - camera.target.y + camera.offset.y,
-                self.radius,
-                PLAYER_COLOR,
-            );
-        }
+        draw_circle(self.position.x, self.position.y, self.radius, PLAYER_COLOR);
     }
 }
