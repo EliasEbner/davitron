60a01041e7279dabff4babb2e6bbc66685fadf85
Elias Ebner <eliase229@gmail.com>
2025-03-07 18:35:39 +0100

    added update camera in player functions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use macroquad::camera::Camera2D;
 use macroquad::color::BLACK;
 use macroquad::math::Vec2;
 use macroquad::window::{
-    clear_background, next_frame, screen_height, screen_width,
+    clear_background, next_frame, screen_height,
+    screen_width,
 };
 
 use crate::entity::Entity;
@@ -14,23 +15,16 @@ mod player;
 #[macroquad::main("MyGame")]
 async fn main() {
     let mut player: Player = Player::new(100f32);
-    let mut camera: Camera2D = Camera2D::default();
+    let mut camera: Camera2D =
+        Camera2D::default();
     camera.offset = Vec2 {
         x: screen_width() * 0.5f32,
         y: screen_height() * 0.5f32,
     };
 
     loop {
-        camera.offset = Vec2 {
-            x: screen_width() * 0.5f32 + player.speed.x * 2f32,
-            y: screen_height() * 0.5f32 + player.speed.y * 2f32,
-        };
-
-        camera.target = Vec2 {
-            x: player.position.x,
-            y: player.position.y,
-        };
         player.update();
+        player.update_camera(&mut camera);
         clear_background(BLACK);
         player.draw(&camera);
 
--- src/player.rs
+++ src/player.rs
@@ -5,6 +5,7 @@ use macroquad::{
     input::is_key_down,
     math::Vec2,
     shapes::draw_circle,
+    window::{screen_height, screen_width},
 };
 
 const PLAYER_COLOR: Color = RED;
@@ -18,7 +19,9 @@ pub struct Player {
 impl Entity for Player {
     fn update(self: &mut Self) {
         // speed
-        if is_key_down(macroquad::input::KeyCode::Space) {
+        if is_key_down(
+            macroquad::input::KeyCode::Space,
+        ) {
             self.speed.y = 10f32;
         } else {
             self.speed.y = 0f32;
@@ -31,8 +34,10 @@ impl Entity for Player {
 
     fn draw(self: &mut Self, camera: &Camera2D) {
         draw_circle(
-            self.position.x - camera.target.x + camera.offset.x,
-            self.position.y - camera.target.y + camera.offset.y,
+            self.position.x - camera.target.x
+                + camera.offset.x,
+            self.position.y - camera.target.y
+                + camera.offset.y,
             self.radius,
             PLAYER_COLOR,
         );
@@ -46,5 +51,22 @@ impl Player {
             position: Vec2::default(),
             speed: Vec2::default(),
         }
+    }
+
+    pub fn update_camera(
+        self: &Self,
+        camera: &mut Camera2D,
+    ) {
+        camera.offset = Vec2 {
+            x: screen_width() * 0.5f32
+                + self.speed.x * 2f32,
+            y: screen_height() * 0.5f32
+                + self.speed.y * 2f32,
+        };
+
+        camera.target = Vec2 {
+            x: self.position.x,
+            y: self.position.y,
+        };
     }
 }
